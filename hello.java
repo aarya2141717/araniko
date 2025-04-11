b50adbcffa2e83507f8aa0d742f01ec499db71c4
aarya2141717 <aaryathapa870@gmail.com>
2025-04-11 10:40:31 +0545

    Addes print statement intest.java from feature branch

--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Aarya");
+        System.out.println("Hello from feature-branch!");
+        System.out.println("Hello from feature-branch-2!");
     }
+    
     
 }
